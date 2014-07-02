c97d4fc5fbe47a276d9860e839cd9544ebfb9a50
iorlov <nchanged@gmail.com>
2014-07-02 23:09:29 +0300

    visible attribute refactor

--- src/src/attributes/visible.js
+++ src/src/attributes/visible.js
@@ -2,38 +2,24 @@ var Wires = Wires || {};
 Wires.attrs = Wires.attrs || {};
 (function() {
 	'use strict';
-	Wires.attrs.visible = Wires.Attr.extend({
-		initialize : function(scope, dom, element, attr) {
-			this.scope = scope;
-			this.instance = scope.instance;
-			this.element = element;
-			this.attr = attr;
-			this.condition = this.attr.value;
-			this.addAttibute = false;
-			this.bindEssentials(this.condition);
-		},
+	Wires.attrs.visible = Wires.BaseAttribute.extend({
 		setValue : function(newVariable, newValue) {
-
 			var result = this.executeStatement(newVariable, newValue, 'return');
-
 			// If animation was set
 			var animation = $(this.element).data('animation');
 			var el = $(this.element);
 			switch (animation) {
-				case 'slide':
-					result ? el.slideDown() : el.slideUp();
+			case 'slide':
+				result ? el.slideDown() : el.slideUp();
 				break;
 			case 'fade':
-					result ? el.fadeIn() : el.fadeOut();
+				result ? el.fadeIn() : el.fadeOut();
 				break;
 			default:
 				// Default is just toggling
 				el.css('display', result ? 'block' : 'none');
 				break;
 			}
-
 		},
-	}, {
-		addAttibute : false
 	});
 })();
