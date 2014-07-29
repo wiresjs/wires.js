399e6c19540f76e61c8c81c0f8929d6da8be9850
iorlov <nchanged@gmail.com>
2014-07-29 12:22:06 +0300

    Grunt task added. Initial build created

---
+++ src/Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+	var targetFiles = [ 
+	    
+	    './lib/htmlparser.js',
+	    './lib/async.js',
+	    
+	    './src/wires-extend.js',
+	    './src/wires-object-utils.js',
+	    
+	    './src/acts/main.js',
+	    './src/acts/toggle.js',
+	    
+	    './src/wires-node.js',
+	    './src/wires-attr.js',
+	    './src/wires-text-node.js',
+	    './src/wires-tag-node.js',
+	    './src/wires-compiler.js',
+	    './src/wires-exec.js',
+	    './src/wires-variable.js',
+	    './src/wires-watcher.js',
+	    './src/wires-base-attr.js',
+	    
+	    './src/attributes/if.js',
+	    './src/attributes/visible.js',
+	    './src/attributes/each.js',
+	    './src/attributes/repeat.js',
+	    './src/attributes/value.js',
+	    './src/attributes/click.js',
+	    './src/attributes/animation.js',
+	    './src/attributes/submit.js',
+	    './src/attributes/selected.js',
+	    
+	    './src/mvc/wires-mvc.js'
+	    
+	];
+
+	grunt.initConfig({
+		uglify : {
+			my_target : {
+				files : {
+					'../build/wires.min.js' : targetFiles
+				}
+			}
+		},
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	//grunt.task.run('uglify');
+	
+	grunt.registerTask('default', ['uglify']);
+
+
+};
