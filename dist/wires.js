75cd9c7468b255f8b480732cffdda8fd99d417b1
Ivan.Orlov <ivan@morrr.com>
2015-08-10 11:05:24 +0300

    Responsive drag events

--- dist/wires.js
+++ dist/wires.js
@@ -2848,6 +2848,21 @@ domain.service("TextNode", ['$evaluate', 'GarbageCollector'],function($evaluate,
 
 (function() {
    var is_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+   var getClientCoords = function(e){
+      var coords = {
+         x : e.clientX,
+         y : e.clientY
+      };
+      if ( e.originalEvent.touches  ){
+         var c;
+         if ( ( c = e.originalEvent.touches[0]) ){
+            coords.x = c.clientX;
+            coords.y = c.clientY;
+         }
+      }
+      return coords;
+   };
+
    domain.service("attrs.ws-drag", ['TagAttribute', '$evaluate'], function(TagAttribute, $evaluate) {
       var WsClick = TagAttribute.extend({
          // Overriding default method
@@ -2865,7 +2880,7 @@ domain.service("TextNode", ['$evaluate', 'GarbageCollector'],function($evaluate,
                      target: ev,
                      watchVariables: false
                   });
-            }
+            };
             // Binding events
 
             var m = window.isMobile;
@@ -2873,30 +2888,27 @@ domain.service("TextNode", ['$evaluate', 'GarbageCollector'],function($evaluate,
                if ( is_firefox ){
                   e.preventDefault();
                }
-               var startCoords = {
-                  x: e.clientX,
-                  y: e.clientY
-               }
+               var startCoords = getClientCoords(e);
                fireEvent({
                   e: e,
                   coords: startCoords,
                   type: "start"
-               })
+               });
                $(this).bind(m ? "touchmove" : "mousemove", function(e) {
-                  var x = startCoords.x - e.clientX;
-                  var y = startCoords.y - e.clientY
+                  var x = startCoords.x - getClientCoords(e).x;
+                  var y = startCoords.y - getClientCoords(e).y;
                   var coords = {
                      x: x,
                      y: y,
                      dy: y < 0 ? "down" : "up",
                      dx: x < 0 ? "right" : "left"
-                  }
+                  };
                   fireEvent({
                      e: e,
                      coords: coords,
                      type: "move"
                   });
-               })
+               });
                $(this).bind(m ? "touchend touchleave touchcancel" : "mouseup", function(e) {
                   if ( is_firefox ){
                      e.preventDefault();
--- src/attributes/ws-drag.js
+++ src/attributes/ws-drag.js
@@ -1,5 +1,20 @@
 (function() {
    var is_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+   var getClientCoords = function(e){
+      var coords = {
+         x : e.clientX,
+         y : e.clientY
+      };
+      if ( e.originalEvent.touches  ){
+         var c;
+         if ( ( c = e.originalEvent.touches[0]) ){
+            coords.x = c.clientX;
+            coords.y = c.clientY;
+         }
+      }
+      return coords;
+   };
+
    domain.service("attrs.ws-drag", ['TagAttribute', '$evaluate'], function(TagAttribute, $evaluate) {
       var WsClick = TagAttribute.extend({
          // Overriding default method
@@ -17,7 +32,7 @@
                      target: ev,
                      watchVariables: false
                   });
-            }
+            };
             // Binding events
 
             var m = window.isMobile;
@@ -25,30 +40,27 @@
                if ( is_firefox ){
                   e.preventDefault();
                }
-               var startCoords = {
-                  x: e.clientX,
-                  y: e.clientY
-               }
+               var startCoords = getClientCoords(e);
                fireEvent({
                   e: e,
                   coords: startCoords,
                   type: "start"
-               })
+               });
                $(this).bind(m ? "touchmove" : "mousemove", function(e) {
-                  var x = startCoords.x - e.clientX;
-                  var y = startCoords.y - e.clientY
+                  var x = startCoords.x - getClientCoords(e).x;
+                  var y = startCoords.y - getClientCoords(e).y;
                   var coords = {
                      x: x,
                      y: y,
                      dy: y < 0 ? "down" : "up",
                      dx: x < 0 ? "right" : "left"
-                  }
+                  };
                   fireEvent({
                      e: e,
                      coords: coords,
                      type: "move"
                   });
-               })
+               });
                $(this).bind(m ? "touchend touchleave touchcancel" : "mouseup", function(e) {
                   if ( is_firefox ){
                      e.preventDefault();
